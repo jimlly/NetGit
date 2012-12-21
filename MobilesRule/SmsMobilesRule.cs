46ced56823b170a423736dd0b693bddba639e777
jimlly <jimlly@126.com>
2012-12-21 21:44:23 +0800

    q

---
+++ MobilesRule/SmsMobilesRule.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * 文件：SmsMobilesRule.cs
+ * 作者：朱孟峰
+ * 日期：2009-09-17
+ * 描述：短信手机号码验证
+*/
+
+using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace Com.Yuantel.Sms.MobilesRule
+{
+    /// <summary>
+    /// 短信手机号码验证
+    /// </summary>
+    public class SmsMobilesRule
+    {
+        // 手机号码正则表达式
+        private static readonly string mobilesRule = ConfigurationManager.AppSettings["MobilesRule"];
+        // 正则表达式类
+        private static readonly Regex reg = new Regex(mobilesRule);
+
+        /// <summary>
+        /// 验证提供的号码是否符合规则
+        /// </summary>
+        /// <param name="mobiles">号码串</param>
+        /// <returns></returns>
+        public static bool Match(string mobiles)
+        {
+            Match m = reg.Match(mobiles);
+            return m.Success;
+        }
+    }
+}
