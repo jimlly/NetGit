5f3ef758f799b6c2e0e0fa31e751b1a1529f892d
jimlly <jimlly@126.com>
2012-12-21 21:31:28 +0800

    a

---
+++ HL95/ISmsReceive.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Yuantel.HL95
+{
+    public interface ISmsReceive
+    {
+        string ReceiveSms(string Mobiles, string Content, string SpNumber);
+    }
+}
